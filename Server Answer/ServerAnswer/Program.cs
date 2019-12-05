ae8d72ca90fb8205a6db7b9b5634bbd30771b125
alexandriaware <alexandrialware@gmail.com>
2019-12-05 13:54:20 -0500

    got in descending order

--- Server Answer/ServerAnswer/Program.cs
+++ Server Answer/ServerAnswer/Program.cs
@@ -24,9 +24,9 @@ namespace ConsoleApp1
                 "businesses were shuttered, and local governments ground to a halt as Americans scanned the text in vain for a web link to click on. " +
                 "Sources also reported a 450 percent rise in temple rubbing and under - the - breath cursing around this time. " +
                 "\"It demands so much of my time and concentration,\" said Chicago resident Dale Huza, who was confronted by the confusing mound of words " +
-                "early Monday afternoon. \"This large block of text, it expects me to figure everything out on my own, and I hate it.\" " +
+                "early Monday afternoon. \"This large block of text, it expects me to figure everything out on my own, and I hate it.\"" +
                 "\"I've never seen anything like it,\" said Mark Shelton, a high school teacher from St Paul, MN who stared blankly at the page in front of him " +
-                "for several minutes before finally holding it up to his ear. \"What does it want from us?\" " +
+                "for several minutes before finally holding it up to his ear. \"What does it want from us?\"" +
                 "As the public grows more desperate, scholars are working to randomly italicize different sections of the text, hoping the italics " +
                 "will land on the important parts and allow everyone to go on with their day.For now, though, millions of panicked and exhausted " +
                 "Americans continue to repetitively search the single column of print from top to bottom and right to left, looking for even the slightest " +
@@ -36,7 +36,7 @@ namespace ConsoleApp1
                 "There are some, however, who remain unfazed by the virtual hailstorm of alternating consonants and vowels, and are determined to ignore it. " +
                 "\"I'm sure if it's important enough, they'll let us know some other way,\" Detroit local Janet Landsman said. \"After all, it can't be that serious. " +
                 "If there were anything worthwhile buried deep in that block of impenetrable English, it would at least have an accompanying photo of a celebrity " +
-                "or a large humorous title containing a pop culture reference.\" " +
+                "or a large humorous title containing a pop culture reference.\"" +
                 "Added Landsman, \"Whatever it is, I'm pretty sure it doesn't even have a point.\"";
 
 
@@ -44,6 +44,7 @@ namespace ConsoleApp1
             questonText = questonText.Replace(".", "");
             questonText = questonText.Replace("\"", "");
             questonText = questonText.Replace("-", "");
+            questonText = questonText.Replace("?", "");
             var words = questonText.ToLower().Split(' ');
             Dictionary<string, int> repeatedWords = new Dictionary<string, int>();
 
@@ -60,9 +61,13 @@ namespace ConsoleApp1
                 }
             }
 
-            foreach (KeyValuePair<string, int> kvp in repeatedWords)
+            var val = from kvp in repeatedWords
+                      orderby kvp.Value descending
+                      select kvp;
+
+            foreach (KeyValuePair<string, int> kvp in val)
             {
-                Console.WriteLine(kvp.Key + " - " + kvp.Value);
+                Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
             }
 
 
